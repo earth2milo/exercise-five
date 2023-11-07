07b43e68ec283b06cd5dd6fc18bf34e9432ffb00
earth2milo <90530636+earth2milo@users.noreply.github.com>
2023-11-07 09:32:05 -0500

    firebase init

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+
+// dependencies + ports
 const express = require('express'); 
 const app = express();
 const port = 4000;
+const firebase = require("firebase/app");
 
+
+
+
+// config from firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDHaO0BaWKt00b82_N6jqJydBzhAuFgREE",
+    authDomain: "exercise-five-3dc5f.firebaseapp.com",
+    projectId: "exercise-five-3dc5f",
+    storageBucket: "exercise-five-3dc5f.appspot.com",
+    messagingSenderId: "1050243458508",
+    appId: "1:1050243458508:web:48556b1c5e99a418d72969"
+  };
+// call and init firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// page routes
 const indexRoute = require('./routes/index');
 const singlePost = require('./routes/singlePost');
 const createPost = require('./routes/createPost');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express'); 
 const router = express.Router();
-
+// init firestore db
+const firestore = require("firebase/firestore");
+// reference to db 
+const db = firestore.getFirestore();
 
 router.get("/", (req, res) => {
-    res.send("All Posts");
+const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+const postsArray = [];
+
+
+postsQuery
+.then((response) => {
+response.forEach((post) => {
+   console.log(post.data());
+   postsArray.push({id: post.id, ...post.data()});
+});
+res.send(postsArray); 
+}).catch((error) => {
+    console.log(error);
+    return res.send(error);
+    });
 });
 
 module.exports = router;
